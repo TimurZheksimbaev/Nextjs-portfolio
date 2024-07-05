d57e7d7429634b2218fa2b115498dd0fa163f5fe
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-07-05 16:41:18 +0300

    edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+
+module.exports = {
+    basePath: "https://github.com/TimurZheksimbaev/Nextjs-portfolio",
+    assetPrefix: 'https://github.com/TimurZheksimbaev/Nextjs-portfolio',
+};
+
 export default nextConfig;
